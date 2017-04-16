8d18f3bcf1f52c6633938f733717019a7b191083
tvsateesh <sateesh.turlapati@gmail.com>
2017-04-15 22:28:18 -0700

    2DHourGlassBigSum.java
    Context 
    Given a  2D Array, :
    
    1 1 1 0 0 0
    0 1 0 0 0 0
    1 1 1 0 0 0
    0 0 0 0 0 0
    0 0 0 0 0 0
    0 0 0 0 0 0
    We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
    
    a b c
      d
    e f g
    There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values.
    
    Task 
    Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
    
    Note: If you have already solved the Java domain's Java 2D Array challenge, you may wish to skip this challenge.
    
    Input Format
    
    There are  lines of input, where each line contains  space-separated integers describing 2D Array ; every value in  will be in the inclusive range of  to .
    
    Constraints
    
    Output Format
    
    Print the largest (maximum) hourglass sum found in .
    
    Sample Input
    
    1 1 1 0 0 0
    0 1 0 0 0 0
    1 1 1 0 0 0
    0 0 2 4 4 0
    0 0 0 2 0 0
    0 0 1 2 4 0
    Sample Output
    
    19
    Explanation
    
     contains the following hourglasses:
    
    1 1 1   1 1 0   1 0 0   0 0 0
      1       0       0       0
    1 1 1   1 1 0   1 0 0   0 0 0
    
    0 1 0   1 0 0   0 0 0   0 0 0
      1       1       0       0
    0 0 2   0 2 4   2 4 4   4 4 0
    
    1 1 1   1 1 0   1 0 0   0 0 0
      0       2       4       4
    0 0 0   0 0 2   0 2 0   2 0 0
    
    0 0 2   0 2 4   2 4 4   4 4 0
      0       0       2       0
    0 0 1   0 1 2   1 2 4   2 4 0
    The hourglass with the maximum sum () is:
    
    2 4 4
      2
    1 2 4
    Submissions: 73411
    Max Score: 15
    Difficulty: Easy
    Rate This Challenge:
        
    More
    Current Buffer (saved locally, editable)

---
+++ 2DHourGlassBigSum.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class 2DHourGlassBigSum {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int arr_i=0; arr_i < 6; arr_i++){
+            for(int arr_j=0; arr_j < 6; arr_j++){
+                arr[arr_i][arr_j] = in.nextInt();
+            }
+        }
+        int sum =Integer.MIN_VALUE; ;
+        for(int arr_i=0; arr_i <= 3; arr_i++){
+            for(int arr_j=0; arr_j <= 3; arr_j++){
+                //00,01,02
+                //11
+                //10,11,12                
+                int currentSum = arr[arr_i][arr_j] +  arr[arr_i][arr_j+1]+  arr[arr_i][arr_j+2] + arr[arr_i+1][arr_j+1] +
+                    arr[arr_i+2][arr_j] +  arr[arr_i+2][arr_j+1]+  arr[arr_i+2][arr_j+2];
+                if ( currentSum > sum ) {
+                    sum = currentSum;
+                }
+            }
+            
+        }
+        System.out.println(sum);
+    }
+}
